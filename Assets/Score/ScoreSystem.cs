6ead3cf1a6ff9a8becfac585eb3284028f804138
jeremyVienne <jere.vienne@gmail.com>
2018-03-12 19:58:55 +0100

    gestion score

---
+++ Assets/Score/ScoreSystem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ScoreSystem : MonoBehaviour {
+
+
+    public Text score_text;
+    public static int score =0 ;    
+    
+
+
+    void Update()
+    {
+        
+        score_text.text = "" + score ;
+    }
+
+
+    public void SetScore(int pt) {
+
+        score += pt;
+        score_text.text = "" + score;
+            
+        
+	}
+
+
+
+
+
+}
---
+++ Assets/Score/ThrowObject.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityScript;
+
+public class ThrowObject : MonoBehaviour
+{
+    public Rigidbody bullet  ;
+    public readonly  Rigidbody clone;
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+
+
+            //clone = Instantiate(bullet , new Vector3(0,0,0), transform.rotation) as Rigidbody;
+            bullet.MovePosition(new Vector3(0, 0, 0));
+            bullet.velocity = transform.TransformDirection(Vector3.forward * 20);
+
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print(collision.collider.name);
+        GetComponent<ScoreSystem>().SetScore(5);
+    }
+}
